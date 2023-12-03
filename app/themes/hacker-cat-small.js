1e2cd7f3ab0d012badaad36a25dc3c45a1b04baa
Sasha Thomas <thoma987@purdue.edu>
2023-12-03 01:20:57 -0800

    fixed cat image url hopefully

--- app/themes/hacker-cat-small.js
+++ app/themes/hacker-cat-small.js
@@ -8,5 +8,5 @@ export default {
   errorOutputColor: '#ffa1a1',
   promptSymbolColor: '#20C90E',
   fontSize: '12px',
-  url: "url('../../public/cat_licensed2.webp')"
+  url: "url('https://media.discordapp.net/attachments/662820218741063723/1180800615140565073/cat_licensed2.beb2bac004c41f4e9e1b.webp?ex=657ebd44&is=656c4844&hm=348afe35f0e12527d74e4dc5cc55bd03f855a02a69e88eb541f55c74cfef38b8&=&width=1156&height=686')"
 };
